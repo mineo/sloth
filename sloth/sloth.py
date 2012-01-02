abb897d8c8fd205f69ccb62d07a443647488d433
Wieland Hoffmann <themineo@gmail.com>
2012-01-02 14:26:30 +0100

    assign queuehandler and downloader after build a QtCore.QSettings object

--- sloth/sloth.py
+++ sloth/sloth.py
@@ -9,9 +9,9 @@ class Sloth(QtGui.QApplication):
         sys.modules["PyQt4.QtGui"].__dict__["qApp"] = self
         QtGui.QApplication.__init__(self, sys.argv)
 
-        self.queuehandler = QueueHandler()
-        self.downloader = Downloader()
-
         QtCore.QCoreApplication.setOrganizationName("RP")
         QtCore.QCoreApplication.setApplicationName("Sloth")
         self.config = QtCore.QSettings()
+
+        self.queuehandler = QueueHandler()
+        self.downloader = Downloader()
