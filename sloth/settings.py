c79e8bb19f5a53af4858a4057611427a12335ee4
Wieland Hoffmann <themineo@gmail.com>
2012-01-07 13:36:14 +0100

    add settings.py

---
+++ sloth/settings.py
@@ -0,0 +1,23 @@
+from .ui import settings
+from PyQt4 import QtGui, QtCore
+
+class SettingsDialog(QtGui.QDialog, settings.Ui_dialog):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        self.setupUi(self)
+        self.accepted.connect(self.update_settings)
+        config = QtCore.QSettings()
+        self.server.setText(config.value("server").toString())
+        self.port.setText(config.value("port", 21).toString())
+        self.user.setText(config.value("user").toString())
+        self.password.setText(config.value("password").toString())
+        self.feed.setText(config.value("feed").toString())
+
+    def update_settings(self):
+        config = QtCore.QSettings()
+        config.setValue("server", self.server.text())
+        config.setValue("port", self.port.text())
+        config.setValue("user", self.user.text())
+        config.setValue("password", self.password.text())
+        config.setValue("feed", self.feed.text())
+        config.sync()
